8cf7e46f6c645f3816cc7ef0ee71442313bbc449
Tariku Bewket <tarikubew@gmail.com>
2024-12-22 15:27:19 +0300

    add input validation on server side

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,14 +1,30 @@
-import { redirect } from 'next/navigation';
-
-import { storePost } from '@/lib/posts';
-import FormSubmit from '@/components/form-submit';
+import PostForm from '@/components/post-form';
 
 export default function NewPostPage() {
-  async function createPost(formData) {
+  async function createPost(prvState, formData) {
     'use server';
+
     const title = formData.get('title');
     const image = formData.get('image');
     const content = formData.get('content');
+
+    let errors = [];
+
+    if (!title || title.trim() === '') {
+      errors.push('Title is required');
+    }
+
+    if (!content || content.trim() === '') {
+      errors.push('Content is required');
+    }
+
+    if (!image || image.size === 0) {
+      errors.push('Image is required');
+    }
+
+    if (errors.length > 0) {
+      return { errors };
+    }
 
     await storePost({
       imageUrl: '',
@@ -20,31 +36,5 @@ export default function NewPostPage() {
     redirect('/feed');
   }
 
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={createPost}>
-        <p className='form-control'>
-          <label htmlFor='title'>Title</label>
-          <input type='text' id='title' name='title' />
-        </p>
-        <p className='form-control'>
-          <label htmlFor='image'>Image URL</label>
-          <input
-            type='file'
-            accept='image/png, image/jpeg'
-            id='image'
-            name='image'
-          />
-        </p>
-        <p className='form-control'>
-          <label htmlFor='content'>Content</label>
-          <textarea id='content' name='content' rows='5' />
-        </p>
-        <p className='form-actions'>
-          <FormSubmit />
-        </p>
-      </form>
-    </>
-  );
+  return <PostForm action={createPost} />;
 }
