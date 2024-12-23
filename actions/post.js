2e71021a53566cce4898a8055a42dc715bc4549f
Tariku Bewket <tarikubew@gmail.com>
2024-12-23 10:12:20 +0300

    revalidate page with latest data to avoid caches with old data

--- actions/post.js
+++ actions/post.js
@@ -1,6 +1,7 @@
 'use server';
 
 import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
 
 import { storePost, updatePostLikeStatus } from '@/lib/posts';
 import { uploadImage } from '@/lib/cloudinary';
@@ -48,5 +49,6 @@ export async function createPost(prvState, formData) {
 }
 
 export async function togglePostLikeStatus(postId) {
-  updatePostLikeStatus(postId, 2);
+  await updatePostLikeStatus(postId, 2);
+  revalidatePath('/', 'layout');
 }
